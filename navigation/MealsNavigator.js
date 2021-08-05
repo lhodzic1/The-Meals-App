d928560c024544d218e272b53c917f96ea0bc3e2
lhodzic1 <lhodzic1@etf.unsa.ba>
2021-08-05 21:47:16 +0200

    categories screen upgraded

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,12 +1,23 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
+import { Platform } from 'react-native';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailsScreen from '../screens/MealDetailsScreen';
+import Colors from '../constants/Colors';
 
 const MealsNavigator = createStackNavigator({
-    Categories: CategoriesScreen,
+    Categories: {
+        screen: CategoriesScreen,
+        navigationOptions: {
+            headerTitle: 'Meal Categories',
+            headerStyle: {
+                backgroundColor: Platform.OS === "android" ? Colors.primaryColor : ' '
+            },
+            headerTintColor: Platform.OS === "android" ? 'white' : Colors.primaryColor
+        }
+    },
     CategoryMeals: CategoryMealsScreen,
     MealDetails: MealDetailsScreen
 });
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button, FlatList, TouchableOpacity } from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
 
 const CategoriesScreen = props => {
 
+  const renderGridItem = (itemData) => {
+    return (
+      <TouchableOpacity style={styles.gridItem} onPress={() => {
+        props.navigation.navigate({ routeName: 'CategoryMeals' });
+      }}>
+        <View >
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
 
   return (
-    <View style={styles.screen}>
-      <Text>Categories Screen!</Text>
-      <Button title="Go to Meals!" onPress={() => {
-        props.navigation.navigate({ routeName: 'CategoryMeals' });
-      }} />
-    </View>
+    <FlatList
+      numColumns={2}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+    />
   );
-}
+};
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150
   }
 });
 
