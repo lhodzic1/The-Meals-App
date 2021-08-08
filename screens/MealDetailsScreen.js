0336cee076ba71adb0cdc8d577b161e58df03d34
lhodzic1 <lhodzic1@etf.unsa.ba>
2021-08-08 11:44:53 +0200

    Details Screen done

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,18 +1,41 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, Image, View, StyleSheet, ScrollView } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../components/HeaderButton';
+import DefaultText from '../components/DefaultText';
 import { MEALS } from '../data/dummy-data';
+
+const ListItem = props => {
+  return (
+    <View style={styles.listItem}>
+      <DefaultText>{props.children}</DefaultText>
+    </View>
+  );
+
+};
 
 const MealDetailsScreen = props => {
   const mealId = props.navigation.getParam('mealId');
   const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
   return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>{selectedMeal.title}</Text>
-    </View>
+    <ScrollView>
+      <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+      <View style={styles.details}>
+        <DefaultText>{selectedMeal.duration} m</DefaultText>
+        <DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
+        <DefaultText>{selectedMeal.affordability}</DefaultText>
+      </View>
+      <Text style={styles.title}>Ingredients</Text>
+      {selectedMeal.ingredients.map(ingredient =>
+        <ListItem key={ingredient}>{ingredient}</ListItem>
+      )}
+      <Text style={styles.title}>Steps</Text>
+      {selectedMeal.steps.map(step =>
+        <ListItem key={step}>{step}</ListItem>
+      )}
+    </ScrollView>
   );
 }
 
@@ -22,24 +45,37 @@ MealDetailsScreen.navigationOptions = navigationData => {
 
   return {
     headerTitle: selectedMeal.title,
-    headerRight: () => 
+    headerRight: () =>
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
         <Item
           title='Favourite'
           iconName='ios-star'
           onPress={() => { }} />
-      </HeaderButtons> 
+      </HeaderButtons>
   };
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+  image: {
+    width: '100%',
+    height: 200
+  },
+  details: {
+    flexDirection: 'row',
+    padding: 15,
+    justifyContent: 'space-around'
   },
   title: {
-    fontFamily: 'open-sans-bold'
+    fontFamily: 'open-sans-bold',
+    fontSize: 22,
+    textAlign: 'center'
+  },
+  listItem: {
+    marginVertical: 5,
+    marginHorizontal: 20,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 10
   }
 });
 
